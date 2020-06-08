eefef5ef964dc840026136701458a16ccdeb0e3c
Alex <alexmoberhofer@gmail.com>
2020-06-08 19:27:22 -0400

    update sdl setup script to download mixer mingw headers

--- sdl2-setup.sh
+++ sdl2-setup.sh
@@ -1,5 +1,7 @@
 #########
 # SDL2 Setup Script for Windows build and executable
+# SDL2 version 2.0.12 stable
+# SDL_mixer version 2.0.4 stable
 # (C) 2020 Alex Oberhofer
 # GPLv2
 #########
@@ -18,6 +20,16 @@ wget https://www.libsdl.org/release/SDL2-devel-2.0.12-mingw.tar.gz
 tar -xvzf SDL2-devel-2.0.12-mingw.tar.gz
 
 rm SDL2-devel-2.0.12-mingw.tar.gz
+
+#mingw mixer headers
+
+wget https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-devel-2.0.4-mingw.tar.gz
+
+tar -xvzf SDL2_mixer-devel-2.0.4-mingw.tar.gz
+
+rm SDL2_mixer-devel-2.0.4-mingw.tar.gz
+
+cd ../..
 
 #SDL dll
 wget https://www.libsdl.org/release/SDL2-2.0.12-win32-x64.zip
