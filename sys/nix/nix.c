47295ee0d211ecd1c368f70a9ad2efa87c8ab5ba
Alex Oberhofer <alexmoberhofer@gmail.com>
2021-05-16 17:18:41 -0400

    use current directory for rc directory + save directory

--- sys/nix/nix.c
+++ sys/nix/nix.c
@@ -20,8 +20,6 @@
 #include "defs.h"
 #include "rc.h"
 
-#define DOTDIR ".gnuboy"
-
 void *sys_timer()
 {
 	Uint32 *tv;
@@ -51,37 +49,17 @@ void sys_sleep(int us)
 
 void sys_checkdir(char *path, int wr)
 {
-	char *p;
-	if (access(path, X_OK | (wr ? W_OK : 0)))
-	{
-		if (!access(path, F_OK))
-			die("cannot access %s: %s\n", path, strerror(errno));
-		p = strrchr(path, '/');
-		if (!p) die("descended to root trying to create dirs\n");
-		*p = 0;
-		sys_checkdir(path, wr);
-		*p = '/';
-		if (mkdir(path, 0777))
-			die("cannot create %s: %s\n", path, strerror(errno));
-	}
 }
 
 void sys_initpath()
 {
-	char *buf, *home = getenv("HOME");
-	if (!home)
-	{
-		buf = ".";
-		rc_setvar("rcpath", 1, &buf);
-		rc_setvar("savedir", 1, &buf);
-		return;
-	}
-	buf = malloc(strlen(home) + strlen(DOTDIR) + 8);
-	sprintf(buf, "%s/" DOTDIR ":.", home);
-	rc_setvar("rcpath", 1, &buf);
-	sprintf(buf, "%s/" DOTDIR "/saves" , home);
-	rc_setvar("savedir", 1, &buf);
-	free(buf);
+    char *buf = ".";
+
+    if (rc_getstr("rcpath") == NULL)
+        rc_setvar("rcpath", 1, &buf);
+
+    if (rc_getstr("savedir") == NULL)
+        rc_setvar("savedir", 1, &buf);
 }
 
 void sys_sanitize(char *s)
