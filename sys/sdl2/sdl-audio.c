656ed748cfb83b79cb91322fd3ac54e73fda94d1
Alex Oberhofer <alexmoberhofer@gmail.com>
2021-06-02 20:51:20 -0400

    fix sound/silent ifdef hell. Back to using an RC variable

--- sys/sdl2/sdl-audio.c
+++ sys/sdl2/sdl-audio.c
@@ -21,80 +21,68 @@
 
 struct pcm pcm;
 
-#ifndef SOUND
-#define SILENT
-#endif
-
-#ifdef SILENT
-static int sound = 0;
-#endif
-
-#ifdef SOUND
-static int sound = 1;
-#endif
 static int samplerate = 44100;
 static int stereo = 1;
+static int sound = 0;
 static volatile int audio_done;
 SDL_AudioDeviceID device;
 
 static int audio_started = 0;
 
 rcvar_t pcm_exports[] =
-	{
-#ifdef SOUND
-		RCV_BOOL("sound", &sound),
-#endif
-		RCV_INT("stereo", &stereo),
-		RCV_INT("samplerate", &samplerate),
-		RCV_END};
+{
+	RCV_BOOL("sound", &sound),
+	RCV_INT("stereo", &stereo),
+	RCV_INT("samplerate", &samplerate),
+	RCV_END
+};
 
 static void audio_callback(void *blah, byte *stream, int len)
 {
-#ifdef SOUND
 	memcpy(stream, pcm.buf, len);
-#endif
 	audio_done = 1;
 }
 
 void pcm_init()
 {
-#ifdef SOUND
-	int i;
-	SDL_AudioSpec want, obtained;
+	if(sound) 
+	{
+		SDL_AudioSpec want, obtained;
 
-	SDL_InitSubSystem(SDL_INIT_AUDIO);
+		SDL_InitSubSystem(SDL_INIT_AUDIO);
 
-	want.freq = samplerate;
-	want.format = AUDIO_U8;
-	want.channels = 1 + stereo;
-	want.samples = samplerate / 60;
-	for (i = 1; i < want.samples; i <<= 1)
-		;
-	want.samples = i;
-	want.callback = audio_callback;
+		want.freq = samplerate;
+		want.format = AUDIO_U8;
+		want.channels = 1 + stereo;
+		want.samples = samplerate / 60;
+		int i;
+		for (i = 1; i < want.samples; i <<= 1)
+			;
+		want.samples = i;
+		want.callback = audio_callback;
 
-	device = SDL_OpenAudioDevice(NULL, 0, &want, &obtained, 0);
+		device = SDL_OpenAudioDevice(NULL, 0, &want, &obtained, 0);
 
-	pcm.hz = obtained.freq;
-	pcm.stereo = obtained.channels - 1;
-	pcm.len = obtained.size;
-	pcm.buf = malloc(pcm.len);
-	pcm.pos = 0;
-	memset(pcm.buf, 0, pcm.len);
-#endif
+		pcm.hz = obtained.freq;
+		pcm.stereo = obtained.channels - 1;
+		pcm.len = obtained.size;
+		pcm.buf = malloc(pcm.len);
+		pcm.pos = 0;
+		memset(pcm.buf, 0, pcm.len);
+	}
 	
 }
 
 int pcm_submit()
 {
-#ifdef SOUND
 	
 	if (!pcm.buf)
 		return 0;
 	if (pcm.pos < pcm.len)
 		return 1;
 
-	if(audio_started == 0) { 
+	if(sound && audio_started == 0) 
+	{ 
 		SDL_PauseAudioDevice(device, 0);
 		audio_started = 1;
 	}
@@ -105,18 +93,11 @@ int pcm_submit()
 	pcm.pos = 0;
 
 	return 1;
-#endif
-
-#ifdef SILENT
-	return 0;
-#endif
 	
 }
 
 void pcm_close()
 {
-#ifdef SOUND
 	if (sound)
 		SDL_CloseAudioDevice(device);
-#endif
 }
