88648284969a05e2188a2b04ce55ebdcd570da8c
Alex Oberhofer <alexmoberhofer@gmail.com>
2021-06-02 21:02:59 -0400

    code cleanup / error checks to audio

--- sys/sdl2/sdl-audio.c
+++ sys/sdl2/sdl-audio.c
@@ -13,9 +13,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
 #include <SDL2/SDL.h>
-
 #include "rc.h"
 #include "pcm.h"
 
@@ -24,10 +22,10 @@ struct pcm pcm;
 static int samplerate = 44100;
 static int stereo = 1;
 static int sound = 0;
-static volatile int audio_done;
-SDL_AudioDeviceID device;
-
 static int audio_started = 0;
+
+static volatile int audio_done;
+static SDL_AudioDeviceID device;
 
 rcvar_t pcm_exports[] =
 {
@@ -37,6 +35,7 @@ rcvar_t pcm_exports[] =
 	RCV_END
 };
 
+//Callback for SDL AudioSpec 
 static void audio_callback(void *blah, byte *stream, int len)
 {
 	memcpy(stream, pcm.buf, len);
@@ -49,19 +48,27 @@ void pcm_init()
 	{
 		SDL_AudioSpec want, obtained;
 
-		SDL_InitSubSystem(SDL_INIT_AUDIO);
+		if(SDL_InitSubSystem(SDL_INIT_AUDIO) < 0) 
+		{
+			printf("SDL Audio failed: %s\n", SDL_GetError());
+			exit(1);
+		}
 
 		want.freq = samplerate;
 		want.format = AUDIO_U8;
 		want.channels = 1 + stereo;
 		want.samples = samplerate / 60;
 		int i;
-		for (i = 1; i < want.samples; i <<= 1)
-			;
+		for (i = 1; i < want.samples; i <<= 1);
 		want.samples = i;
 		want.callback = audio_callback;
 
 		device = SDL_OpenAudioDevice(NULL, 0, &want, &obtained, 0);
+
+		if(device == 0) {
+			printf("SDL could not open audio device: %s\n", SDL_GetError());
+			exit(1);
+		}
 
 		pcm.hz = obtained.freq;
 		pcm.stereo = obtained.channels - 1;
